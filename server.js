ecd92f2741f845b3a4e5b18d515f64be6bec3e7e
TPhantom <fadlymwb0@gmail.com>
2022-05-29 11:14:19 +0700

    Initial Author Routes

--- server.js
+++ server.js
@@ -2,14 +2,17 @@ const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 require('dotenv').config()
 const app = express();
+const bodyParser = new require('body-parser')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
@@ -18,5 +21,6 @@ db.on('error', error => console.error(error))
 db.on('open', () => console.log('Connected to mongoose'))
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
